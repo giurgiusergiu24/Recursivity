dfffb74c8171a1c9b043745e82fdc7702570f626
giurgiusergiu24 <giurgiusergiu93@gmail.com>
2021-02-15 20:31:16 +0200

    Problema_Bloc_Curs_16

--- Curs_16/src/com/company/itschool/Main.java
+++ Curs_16/src/com/company/itschool/Main.java
@@ -1,10 +1,29 @@
 package com.company.itschool;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
 
+        Bloc blocA1 = new Bloc(12,"Scara 1");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Care este numele blocului?");
+        String numeBloc = scanner.nextLine(); //E2
+        blocA1.setNumeBloc(numeBloc);
+        String adresa = blocA1.getAdresa();
+        System.out.println(adresa);
+
+        System.out.println("Introduceti pretul pe etaj: ");
+        int pret = scanner.nextInt();
+        scanner.nextLine();
+        blocA1.setPretPeEtaj(pret);
+
+        int total = blocA1.calculateTotalPrice();
+        System.out.println("Te costa: " + total);
+
 
     }
+
 }
